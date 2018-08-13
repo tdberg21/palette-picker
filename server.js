574b6f0897f80ff93cd43f9224ecb2bf8789386e
Tory Dannenberg <rfdberg@gmail.com>
2018-08-13 17:43:42 -0600

    Configure path to use for static assets

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('public'));
+
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
 
-app.get('/', (requestAnimationFrame, response) => {
-  response.send('Oh hey Palette Picker');
+app.get('/', (request, response) => {
 });
 
 app.listen(app.get('port'), () => {
