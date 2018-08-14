ba8fb581d71cab52a752192f59a35fc5f68fcacf
Tory Dannenberg <rfdberg@gmail.com>
2018-08-13 18:37:36 -0600

    Generate random colors for palettes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,12 +1,25 @@
-console.log('hello world')
 
-const handleLock = () => {
-  console.log('click')
+const randomPaletteGenerator = () => {
+  let colors = [];
+
+  for(let i = 1; i <= 5; i++) {
+    colors.push('#' + Math.floor(Math.random()*16777215).toString(16))
+  }
+
+  colors.forEach((color, index) => {
+    $(`.color-box${index + 1}`).css('background-color', color)
+  })
 }
 
+const handleLock = (boxNumber) => {
+  console.log(boxNumber)
+  $(`.lock-button${boxNumber}`).toggleClass('locked');
+}
 
-$('.lock-button1').click(() => handleLock())
-$('.lock-button2').click(() => handleLock())
-$('.lock-button3').click(() => handleLock())
-$('.lock-button4').click(() => handleLock())
-$('.lock-button5').click(() => handleLock())
+randomPaletteGenerator();
+$('.lock-button1').click(() => handleLock(1))
+$('.lock-button2').click(() => handleLock(2))
+$('.lock-button3').click(() => handleLock(3))
+$('.lock-button4').click(() => handleLock(4))
+$('.lock-button5').click(() => handleLock(5))
+$('.new-palette-button').click(randomPaletteGenerator);
