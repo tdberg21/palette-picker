59d40eb063b56a08429000def81d3a35697eb868
Tory Dannenberg <rfdberg@gmail.com>
2018-08-14 12:39:31 -0600

    Add endpoint to post new projects

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.get('/api/v1/projects', (request, response) => {
   const projects = app.locals.projects;
 
   response.json(projects);
-})
+});
 
 app.get('/api/v1/projects/:id', (request, response) => {
   const { id } = request.params;
@@ -30,16 +30,22 @@ app.get('/api/v1/projects/:id', (request, response) => {
   } else {
     return response.sendStatus(404);
   }
-})
+});
 
-// app.post('/api/v1/projects/new', (request, response) => {
-//   const { project } = request.body;
-//   const id = Date.now();
-
-//   app.locals.projects.push({id, project_name: project});
-//   response.status(201).json({ id, project_name: project });
-// })
+app.post('/api/v1/projects/new', (request, response) => {
+  console.log(request.body)
+  const project = request.body;
+  const id = Date.now();
+  if (!project.project_name) {
+    response.status(422).send({
+      error: 'no project name provided'
+    });
+  } else {
+    app.locals.projects.push({id, project_name: project.project_name});
+    response.status(201).json({id, project_name: project.project_name});
+  }
+});
 
 app.listen(app.get('port'), () => {
-  console.log(`${app.locals.title} is running on ${app.get('port')}.`)
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
 });
