8a114dd6baea35a91785247465bb9dce5b8c4c7c
Tory Dannenberg <rfdberg@gmail.com>
2018-08-13 19:01:06 -0600

    Update styles for lower sections, add handleSavePalette function

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,6 +1,7 @@
+let colors = [];
 
 const randomPaletteGenerator = () => {
-  let colors = [];
+  colors = [];
 
   for(let i = 1; i <= 5; i++) {
     colors.push('#' + Math.floor(Math.random()*16777215).toString(16))
@@ -8,12 +9,18 @@ const randomPaletteGenerator = () => {
 
   colors.forEach((color, index) => {
     $(`.color-box${index + 1}`).css('background-color', color)
+    $(`.lock-button${index + 1}`).css('background-color', color)
   })
 }
 
 const handleLock = (boxNumber) => {
   console.log(boxNumber)
   $(`.lock-button${boxNumber}`).toggleClass('locked');
+}
+
+const handleSavePalette = () => {
+  let paletteName = $('.palette-name-input').val();
+  console.log(paletteName, colors);
 }
 
 randomPaletteGenerator();
@@ -23,3 +30,4 @@ $('.lock-button3').click(() => handleLock(3))
 $('.lock-button4').click(() => handleLock(4))
 $('.lock-button5').click(() => handleLock(5))
 $('.new-palette-button').click(randomPaletteGenerator);
+$('.save-palette-button').click(handleSavePalette);
