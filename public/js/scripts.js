e6af78f8d8120f9c776380f827b8fd4e4f63eba3
Tory Dannenberg <rfdberg@gmail.com>
2018-08-14 12:06:06 -0600

    Add hex codes to the color boxes, update css

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -4,30 +4,36 @@ const randomPaletteGenerator = () => {
   colors = [];
 
   for(let i = 1; i <= 5; i++) {
-    colors.push('#' + Math.floor(Math.random()*16777215).toString(16))
+    if (!$(`.lock-button${i}`).hasClass('locked')) {
+      colors.push('#' + Math.floor(Math.random()*16777215).toString(16))
+    } else {
+      colors.push(colors[i]);
+    }
   }
 
   colors.forEach((color, index) => {
-    $(`.color-box${index + 1}`).css('background-color', color)
-    $(`.lock-button${index + 1}`).css('background-color', color)
+    $(`.color-box${index + 1}`).css('background-color', color);
+    $(`.color-code${index + 1}`).text(color);
+    $(`.lock-button${index + 1}`).css('background-color', color);
   })
 }
 
-const handleLock = (boxNumber) => {
-  console.log(boxNumber)
+const lockColor = (boxNumber) => {
   $(`.lock-button${boxNumber}`).toggleClass('locked');
 }
 
 const handleSavePalette = () => {
   let paletteName = $('.palette-name-input').val();
+  let project_name = $('#project-name-dropdown').val();
   console.log(paletteName, colors);
+  console.log('project name:', project_name);
 }
 
 randomPaletteGenerator();
-$('.lock-button1').click(() => handleLock(1))
-$('.lock-button2').click(() => handleLock(2))
-$('.lock-button3').click(() => handleLock(3))
-$('.lock-button4').click(() => handleLock(4))
-$('.lock-button5').click(() => handleLock(5))
+$('.lock-button1').click(() => lockColor(1));
+$('.lock-button2').click(() => lockColor(2));
+$('.lock-button3').click(() => lockColor(3));
+$('.lock-button4').click(() => lockColor(4));
+$('.lock-button5').click(() => lockColor(5));
 $('.new-palette-button').click(randomPaletteGenerator);
 $('.save-palette-button').click(handleSavePalette);
