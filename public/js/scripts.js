3dec1267aaae1ed683e191ec6288710acfc320a8
Tory Dannenberg <rfdberg@gmail.com>
2018-08-13 18:18:13 -0600

    Add api endpoint for projects using data from app.locals,
    import jquery and add event listeners for the lock buttons

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,1 +1,12 @@
 console.log('hello world')
+
+const handleLock = () => {
+  console.log('click')
+}
+
+
+$('.lock-button1').click(() => handleLock())
+$('.lock-button2').click(() => handleLock())
+$('.lock-button3').click(() => handleLock())
+$('.lock-button4').click(() => handleLock())
+$('.lock-button5').click(() => handleLock())
--- server.js
+++ server.js
@@ -5,9 +5,19 @@ app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Palette Picker';
+app.locals.projects = [
+  { id: '1', project_name: 'Project1' },
+  { id: '2', project_name: 'Project2' }
+];
 
 app.get('/', (request, response) => {
 });
+
+app.get('/api/v1/projects', (request, response) => {
+  const projects = app.locals.projects;
+
+  response.json(projects);
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`)
