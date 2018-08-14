7a84397728fea3539e9ed3ffb048f7a440fb8bb5
Tory Dannenberg <rfdberg@gmail.com>
2018-08-14 12:27:11 -0600

    Install and configure body parser

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.set('port', process.env.PORT || 3000);
