850b6b649ea9a34fe3a22be9253d876f7c8b57ba
Tory Dannenberg <rfdberg@gmail.com>
2018-08-15 14:29:04 -0600

    Refactor project get request to return info from database

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,9 +20,13 @@ app.get('/', (request, response) => {
 });
 
 app.get('/api/v1/projects', (request, response) => {
-  const projects = app.locals.projects;
-
-  response.json(projects);
+  database('projects').select()
+    .then((projects) => {
+      response.status(200).json(projects);
+    })
+    .catch((error) => {
+      response.status(500).json({ error });
+    });
 });
 
 app.get('/api/v1/projects/:id', (request, response) => {
