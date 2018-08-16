7b2043326d1d85d471abed9d3087dd6c1f09c165
Tory Dannenberg <rfdberg@gmail.com>
2018-08-16 15:43:02 -0600

    Fix test file configuration and get first test to pass

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,12 +1,19 @@
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
-const server = require('../server');
+const app = require('../server');
 
 chai.use(chaiHttp);
 
 describe('Client Routes', () => {
-
+  it('should return the homepage', done => {
+    chai.request(app)
+      .get('/api/v1/projects')
+      .end((error, response) => {
+        response.should.have.status(200);
+        done();
+      })
+  });
 });
 
 describe('API Routes', () => {
