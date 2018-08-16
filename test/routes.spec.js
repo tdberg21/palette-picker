bdd86186e5a4c4b996fd9e0df6700e1840d38ade
Tory Dannenberg <rfdberg@gmail.com>
2018-08-16 16:06:38 -0600

    Finish happy path tests for api/v1/projects

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -8,14 +8,33 @@ chai.use(chaiHttp);
 describe('Client Routes', () => {
   it('should return the homepage', done => {
     chai.request(app)
+      .get('/')
+      .end((error, response) => {
+        response.should.have.status(200);
+        response.should.be.html;
+        done();
+      });
+  });
+});
+
+
+describe('API Routes', () => {
+
+describe('GET /api/v1/projects', () => {
+  it('should return the homepage', done => {
+    chai.request(app)
       .get('/api/v1/projects')
       .end((error, response) => {
         response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.should.have.length(2);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+        response.body[0].should.have.property('project_name');
+        response.body[0].project_name.should.equal('apple');
         done();
       })
   });
 });
-
-describe('API Routes', () => {
-
 });
