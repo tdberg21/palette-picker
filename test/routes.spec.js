421e63e3ca29979ca2c4c27532e95ecee45c3eec
Tory Dannenberg <rfdberg@gmail.com>
2018-08-19 13:47:34 -0600

    Add happy and sad path tests for posting new projects

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -3,7 +3,7 @@ const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const app = require('../server');
-
+var knex = require('../db/knex');
 
 chai.use(chaiHttp);
 
@@ -43,7 +43,7 @@ describe('GET /api/v1/projects', () => {
         response.should.have.status(200);
         response.should.be.json;
         response.body.should.be.a('array');
-        response.body.should.have.length(2);
+        response.body.should.have.length(1);
         response.body[0].should.have.property('id');
         response.body[0].id.should.equal(1);
         response.body[0].should.have.property('project_name');
@@ -64,6 +64,35 @@ describe('GET /api/v1/palettes', () => {
         done();
       })
   });
+});
+
+describe('POST /api/v1/projects/new', () => {
+  it('should create a new project', done => {
+    chai.request(app)
+    .post('/api/v1/projects/new')
+    .send({
+      project_name: 'New Project'
+    })
+    .end((error, response) => {
+      response.should.have.status(201);
+      response.should.be.json;
+      response.body.should.be.a('object');
+      response.body.should.have.property('id');
+      response.body.id.should.equal(2);
+      done();
+    })
+  });
+
+  it('should not create a project with missing data', done => {
+    chai.request(app)
+    .post('/api/v1/projects/new')
+    .end((error, response) => {
+      response.should.have.status(422);
+      response.body.should.have.property('error');
+      response.body.error.should.equal(`Expected format: { project_name: <String> }. You're missing a "project_name" property.`);
+      done();
+    })
+  })
 });
 
 
